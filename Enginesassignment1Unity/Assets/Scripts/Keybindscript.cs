3d6f5cbe764a414a6d694217bfe99982f37a6153
Jean-pierre Duchene <100584717@oncampus.local>
2018-09-27 15:55:48 -0400

    UI control change
    UI exists, can change the buttons there to different buttons on the keyboard, but not yet linked to the character's movement yet. Also not able to save changes yet, will add both later.

---
+++ Enginesassignment1Unity/Assets/Scripts/Keybindscript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keybindscript : MonoBehaviour {
+
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+    private GameObject currentKey;
+    private Color32 normal = new Color32(39,171,249,255);
+    private Color32 selected = new Color(239,116,36,255);
+    public Text up, down, left, right;
+	// Use this for initialization
+	void Start () {
+        keys.Add("Up",KeyCode.W);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Left", KeyCode.A);
+        keys.Add("Right", KeyCode.D);
+
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(keys["Up"]))
+        {
+            //Do a move action add this in
+
+        }
+        if (Input.GetKeyDown(keys["Down"]))
+        {
+            //Do a move action add this in
+
+        }
+        if (Input.GetKeyDown(keys["Left"]))
+        {
+            //Do a move action add this in
+
+        }
+        if (Input.GetKeyDown(keys["Right"]))
+        {
+            //Do a move action add this in
+
+        }
+    }
+
+    void OnGUI()
+    {
+        if(currentKey != null)
+        {
+            Event e = Event.current;
+            if(e.isKey)
+            {
+                keys[currentKey.name]= e.keyCode;
+                currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = normal;
+                currentKey = null;
+            }
+        }
+    }
+    public void ChangeKey(GameObject clicked)
+    {
+      if(currentKey != null)
+      {
+            currentKey.GetComponent<Image>().color = normal;
+      }
+
+      currentKey = clicked;
+      currentKey.GetComponent<Image>().color = selected;
+    }
+}
