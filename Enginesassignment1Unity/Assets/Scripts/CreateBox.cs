6d6ff793ee3d7f7b162788ef621adf2046eeba68
jakejandu <jakejandu@hotmail.com>
2018-09-27 00:42:32 -0400

    Cube Trigger
    Able to pass through a "trigger" to spawn cubes from a spawnpoint
    
    "used for Flyweight"

---
+++ Enginesassignment1Unity/Assets/Scripts/CreateBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBox : MonoBehaviour
+{
+
+	public Transform SpawnPoint;
+	public Rigidbody Prefab;
+
+	// Use this for initialization
+	void OnTriggerEnter () {
+
+		Rigidbody RigidPrefab;
+		RigidPrefab = Instantiate (Prefab, SpawnPoint.position, SpawnPoint.rotation)as Rigidbody;
+	}
+}
