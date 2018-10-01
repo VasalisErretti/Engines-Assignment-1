691233101a85c94b8215be2086eb3e1593e53eae
jakejandu <jakejandu@hotmail.com>
2018-10-01 17:33:40 -0400

    Observer + Flyweight Complete + DLL integration

---
+++ Enginesassignment1Unity/Assets/Plugins/itemDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using myDLL;
+using UnityEngine;
+
+public class itemDrop : MonoBehaviour {
+
+
+    public ItemDrop getItem = new ItemDrop();
+    public int n;
+    public int m;
+
+    // Use this for initialization
+    void Start() {
+        n = Random.Range(0, 10);
+        m = Random.Range(0, 10);
+
+        getItem.ItemSuccess(n, m);
+
+        if (getItem.chance == true)
+        {
+            Debug.Log("YOU FOUDN AN ITEM");
+        }
+        else
+        {
+            Debug.Log("No Item Found");
+        }
+	}
+
+	
+
+}
